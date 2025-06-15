93cc6b995fffedb105a8fc0f92ddc039e7a32b74
830Utpal <lakshman2016yadav@gmail.com>
2025-06-15 21:22:28 +0530

    initialisation and yargs for commit revert push pull  completed

---
+++ backend/controllers/revert.js
@@ -0,0 +1,3 @@
+export async function revertRepo() {
+  console.log("Revert command called");
+}
--- backend/index.js
+++ backend/index.js
@@ -1,23 +1,53 @@
 import yargs from 'yargs';
 import { hideBin } from 'yargs/helpers';
 import { initRepo } from './controllers/init.js';
-import { addRepo} from './controllers/add.js';
+import { addRepo } from './controllers/add.js';
+import { commitRepo } from './controllers/commit.js';
+import { pushRepo } from './controllers/push.js';
+import { pullRepo } from './controllers/pull.js';
+import { revertRepo } from './controllers/revert.js';
 
 yargs(hideBin(process.argv))
+  .scriptName('index.js')
+  .command('init', 'Initialise a new repository', {}, initRepo)
+
   .command(
-    'init',
-    'Initialise a new repository',
-    {},
-    initRepo
-  )
-  .command('add <file>', 'Add a file to the repository', (yargs) => {
-    yargs.positional('file', {
-      describe: 'The file to add',
-      type: 'string'
-    });
-  },
+    'add <file>',
+    'Add a file to the repository',
+    (yargs) => {
+      yargs.positional('file', {
+        describe: 'The file to add',
+        type: 'string',
+      });
+    },
     addRepo
-)
+  )
+
+  .command(
+    'commit',
+    'Commit changes to the repository',
+    (yargs) =>
+      yargs.option('message', {
+        alias: 'm',
+        describe: 'Commit message',
+        type: 'string',
+        demandOption: true,
+      }),
+    commitRepo
+  )
+
+  .command('push', 'Push committed changes', {}, pushRepo)
+
+  .command('pull', 'Pull latest changes', {}, pullRepo)
+
+  .command('revert <commitID>', 'Revert to the previous commit', (yargs) => {
+    yargs.positional('commitID', {
+      describe: 'The commit to revert to',
+      type: 'string',
+    });
+  }, revertRepo)
+
   .demandCommand(1, 'You need to specify a command')
+  .strict()
   .help()
   .argv;
